461c70684d3287016c860e5683dba3d6abd28b80
migpig <migpigmig@gmail.com>
2022-05-17 20:46:45 +0900

    2day

--- Framework/Main.cpp
+++ Framework/Main.cpp
@@ -1,71 +1,105 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
 
-using namespace std; //iostream을 쓸 때 는 using namespace std;를 꼭 써줘야한다.
+using namespace std;
 
 
-typedef struct tagInfo
+
+
+struct Vector3 
 {
-	int iNumber;
+	int x = 0, y = 0, z = 0;
 
-	tagInfo()
-	{
-		cout << "기본 생성자" << endl;
-	}
+	Vector3() {};							//기본 생성자 없이 복사 생성자를 만들수 없다.
 
-	tagInfo(int _iNumber) // int _iNumber 받아온 값
-	{
-		iNumber = _iNumber; // _iNumber 받아온 값으로 iNumber 를 초기화
-		cout << "복사 생성자" << endl;
-	}
-}INFO;
+	Vector3(int _x, int _y)
+		: x(_x), y(_y), z(0) {};			//복사 생성자를 여러개 만들수도 있다. 매개변수 2개 여기서 z는 초기화 안해 줄 수 없으니 임의값
+
+	Vector3(int _x, int _y, int _z)
+		: x(_x), y(_y), z(_z) {};			//복사 생성자를 만들때 기본 생성자를 무조건 정의 해줘야한다. 매개변수 3개
+	
+	/* tagVector3(int _x, int _y, float _z) 매개변수의 형태에 따라서 호출되는 복사생성자가 달라짐
+		: x(_x), y(_y), z(_z) {}; */
+};											//vector3은 x 수평 y 수직 z 외적을했을때 생기는 하나의 선 3개의 원소로 이루어져있다.
+											//3d = vector3 2d = vector2
+
+struct Transnsform
+{
+	Vector3 Position;
+	Vector3 Rotation;
+	Vector3 Scale;
+};
+
+struct Object
+{
+	char* Name;
+	int Speed;
+	Transnsform TransInfo;
+};
+
+void Initialize(Object* _Object, char* _Name, int _PosX, int _PosY, int _PosZ);
+char* SetName();
+void Output(Object* _Object);
 
 
 int main(void)
 {
-	//** 출력
-	//printf_s("문자 출력");
-	//cout << "문자 출력"; //뒤에있는 이 문자열을 c 콘솔창으로 out 내보내라 cout
-						 // << 는 쉬프트 연산자 보통 비트연산 할 때 많이 사용 (복잡한 연산자를 사용 할 때 많이 사용) 복잡한 연산을 수월하게 풀어 줄 수 있지만 엄청 빠르진 않다
+	//Object* Player = nullptr;
+	Object* Player = new Object;
+	Initialize(Player,nullptr,10,20,30);
 
-	
-						 
-						 
-						 
-	//** 줄바꿈
-	//printf_s("\n을 사용하면\n줄바꿈");
-	//cout << "endl 을 사용하면" << endl << "줄바꿈"; // end 마지막 l 라인 endl 줄바꿈
+	Object* Enemy = new Object;
+	Initialize(Enemy,(char*)"Enemy",100,200,300);
 
-	
-	
-	
-	
-	//** 입력
-	/*
-	int iNumber = 0;
 
+	Output(Player);
+	Output(Enemy);
+
+	/*Vector3 vPosition = Vector3(10,20);		// 초기화 변수를 2개 호출하면 매개변수 2개짜리 복사생성자가 자동 호출 , 3개는 3개짜리가 자동호출
 	
-	//printf_s("입력: ");
-	//scanf_s("%d", &iNumber);
-
-	//printf_s("출력 : %d",iNumber);
-	
-	cout << "입력: ";
-	cin >> iNumber;
-
-	cout << "출력: " << iNumber;
-	*/
-
-	// ** 동적할당
-	//INFO* pInfo = (INFO*)malloc(sizeof(INFO));		//void 포인터는 4byte void는 데이터 공간의 크기를 가지고 있지만 형태를 가지고 있지 않고 뭐든지 할당 할 수 있다//malloc()은 void*
-													//(siceof(INFO))는 구조체안에 들어갈 자료형의 갯수를 자유롭게 사용 하고 크기를 받아 올 수 있도록 해준다
-	INFO* pInfo = new INFO(10);				// c++에서 malloc은 new로 가능하다 new는 생성자
-	cout << pInfo->iNumber << endl;									// pInfo 를 new INFO로 초기화 하니 컴파일러가 알아서 구조체 안에 있는 생성자를 호출해줌
-
-	//free(pInfo);
-	delete pInfo;
+	cout << vPosition.x << ", " << vPosition.y << endl;*/
 
 
 
 	return 0;
+}
+
+
+void Initialize(Object* _Object, char* _Name, int _PosX, int _PosY, int _PosZ)
+{
+	_Object->Name = (_Name == nullptr) ? SetName() : _Name;
+
+
+	_Object->Speed = 0;
+
+	_Object->TransInfo.Position = Vector3(_PosX, _PosY, _PosZ);
+	_Object->TransInfo.Rotation = Vector3(0, 0, 0);
+	_Object->TransInfo.Scale = Vector3(0, 0, 0);
+}
+
+char* SetName()
+{
+	char Buffer[128] = "";
+	
+	cout << "입력 : ";
+	cin >> Buffer;
+	//char* pName = (char*)malloc(strlen(Buffer) + 1);
+	char* pName = new char[strlen(Buffer) + 1];
+
+	strcpy(pName, Buffer);
+
+	return nullptr;
+}
+
+void Output(Object* _Object)
+{
+	if (_Object->Name != nullptr)
+		cout << "Name : " << _Object->Name << endl;
+
+	cout << "Speed : " << _Object->Speed << endl;
+
+	cout << "X : " << _Object->TransInfo.Position.x <<
+		", Y : " << _Object->TransInfo.Position.y <<
+		", Z : " << _Object->TransInfo.Position.z << endl;
 }
